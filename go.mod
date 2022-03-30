930bda295ad56810d797ec033c981b4a9baaed53
Shikanime Deva <deva.shikanime@protonmail.com>
2022-03-30 18:53:47 +0200

    Refactor using echo

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.13
 
 require (
 	cloud.google.com/go/iam v0.3.0 // indirect
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/echo/v4 v4.7.2
 	github.com/ory/oathkeeper v0.38.23-beta.1
 	github.com/spf13/cobra v1.1.1
 	google.golang.org/api v0.73.0
--- main.go
+++ main.go
@@ -7,25 +7,30 @@ import (
 	"net/http"
 	"strings"
 
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/ory/oathkeeper/pipeline/authn"
 	"github.com/spf13/cobra"
 	"google.golang.org/api/idtoken"
 )
 
+var address string
+var username string
+var password string
+
 var rootCmd = &cobra.Command{
 	Use: "oathkeeper-google",
 	Run: func(cmd *cobra.Command, args []string) {
-		mux := http.NewServeMux()
-		mux.HandleFunc("/hydrators/token", HandleHydrateToken)
-		if err := http.ListenAndServe(address, mux); err != nil {
+		e := echo.New()
+		e.Use(middleware.Logger())
+		e.Use(middleware.Recover())
+		e.Use(middleware.BasicAuth(HandleBasicAuth))
+		e.GET("/hydrators/token", HandleHydrateToken)
+		if err := e.Start(address); err != nil {
 			log.Fatalf("failed to start server: %s", err)
 		}
 	},
 }
-
-var address string
-var username string
-var password string
 
 func init() {
 	rootCmd.Flags().StringVar(&address, "address", ":8080", "address to listen on")
@@ -38,45 +43,33 @@ func main() {
 	log.Fatal(rootCmd.Execute())
 }
 
-func HandleHydrateToken(w http.ResponseWriter, req *http.Request) {
-	basicAuthUser, basicAuthPwd, ok := req.BasicAuth()
-	if !ok {
-		w.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
-		http.Error(w, "Missing Basic Auth", http.StatusUnauthorized)
-		return
-	}
-	userCmp := subtle.ConstantTimeCompare([]byte(basicAuthUser), []byte(username))
-	passCmp := subtle.ConstantTimeCompare([]byte(basicAuthPwd), []byte(password))
-	if userCmp != 1 || passCmp != 1 {
-		http.Error(w, "Credentials mismatch", http.StatusUnauthorized)
-		return
-	}
+func HandleHydrateToken(c echo.Context) error {
+	req := c.Request()
 	var as authn.AuthenticationSession
 	if err := json.NewDecoder(req.Body).Decode(&as); err != nil {
-		http.Error(w, "Masformed body", http.StatusBadRequest)
-		return
+		return c.NoContent(http.StatusBadRequest)
 	}
 	aud := req.URL.Query().Get("audience")
 	if aud == "" {
-		http.Error(w, "Missing audience", http.StatusBadRequest)
-		return
+		return c.NoContent(http.StatusBadRequest)
 	}
 	ts, err := idtoken.NewTokenSource(req.Context(), aud)
 	if err != nil {
-		http.Error(w, "Token source error", http.StatusInternalServerError)
-		return
+		return c.NoContent(http.StatusInternalServerError)
 	}
 	t, err := ts.Token()
 	if err != nil {
-		http.Error(w, "Token fetch error", http.StatusInternalServerError)
-		return
+		return c.NoContent(http.StatusInternalServerError)
 	}
 	as.SetHeader(
 		"Authorization",
 		strings.Join([]string{t.TokenType, t.AccessToken}, " "),
 	)
-	if err := json.NewEncoder(w).Encode(as); err != nil {
-		http.Error(w, "Encoding error", http.StatusInternalServerError)
-		return
-	}
+	return c.JSON(http.StatusOK, as)
+}
+
+func HandleBasicAuth(u, p string, c echo.Context) (bool, error) {
+	userCmp := subtle.ConstantTimeCompare([]byte(u), []byte(username))
+	passCmp := subtle.ConstantTimeCompare([]byte(p), []byte(password))
+	return userCmp == 1 || passCmp == 1, nil
 }
