1a21fddb801913972082a97cdc3f43d30577d749
Shikanime Deva <shikanime.deva@shikanime.studio>
2022-04-27 10:52:00 +0200

    Add audience in URL param

--- main.go
+++ main.go
@@ -6,12 +6,12 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"net/url"
 	"strings"
 	"text/template"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"github.com/mitchellh/mapstructure"
 	"github.com/ory/oathkeeper/pipeline/authn"
 	"github.com/ory/oathkeeper/x"
 	"github.com/spf13/cobra"
@@ -38,7 +38,7 @@ var rootCmd = &cobra.Command{
 		e.Use(middleware.Logger())
 		e.Use(middleware.Recover())
 		e.Use(middleware.BasicAuth(BasicAuthValidator))
-		e.POST("/hydrators/token", HydrateToken(tpl))
+		e.POST("/hydrators/token/audiences/:audience", HydrateToken(tpl))
 		if err := e.Start(address); err != nil {
 			log.Fatalf("failed to start server: %s", err)
 		}
@@ -63,36 +63,33 @@ func HydrateToken(claimtpl *template.Template) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		req := c.Request()
 		ctx := req.Context()
+		aud, err := url.QueryUnescape(c.Param("audience"))
+		if err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, "invalid audience")
+		}
 		var as authn.AuthenticationSession
 		if err := json.NewDecoder(req.Body).Decode(&as); err != nil {
-			return c.NoContent(http.StatusBadRequest)
+			return echo.NewHTTPError(http.StatusBadRequest, "invalid request body")
 		}
 		var tpl bytes.Buffer
 		if err := claimtpl.Execute(&tpl, as); err != nil {
-			return c.NoContent(http.StatusInternalServerError)
+			return echo.NewHTTPError(http.StatusInternalServerError, "failed to execute claims template")
 		}
-		var rres map[string]interface{}
-		if err := json.Unmarshal(tpl.Bytes(), &rres); err != nil {
-			return c.NoContent(http.StatusInternalServerError)
-		}
-		var res struct {
-			Audiences []string               `mapstructure:"aud"`
-			Claims    map[string]interface{} `mapstructure:",remain"`
-		}
-		if err := mapstructure.Decode(rres, &res); err != nil {
-			return c.NoContent(http.StatusBadRequest)
+		var res map[string]interface{}
+		if err := json.Unmarshal(tpl.Bytes(), &res); err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, "failed to unmarshal claims template")
 		}
 		creds, err := google.FindDefaultCredentials(ctx)
 		if err != nil {
-			return c.NoContent(http.StatusInternalServerError)
+			return echo.NewHTTPError(http.StatusInternalServerError, "failed to find default credentials")
 		}
-		ts, err := idtoken.NewTokenSource(ctx, res.Audiences[0], option.WithAudiences(res.Audiences...), option.WithCredentials(creds))
+		ts, err := idtoken.NewTokenSource(ctx, aud, option.WithCredentials(creds), idtoken.WithCustomClaims(res))
 		if err != nil {
-			return c.NoContent(http.StatusInternalServerError)
+			return echo.NewHTTPError(http.StatusInternalServerError, "failed to create token source")
 		}
 		t, err := ts.Token()
 		if err != nil {
-			return c.NoContent(http.StatusInternalServerError)
+			return echo.NewHTTPError(http.StatusInternalServerError, "failed to get token")
 		}
 		as.SetHeader(
 			"Authorization",
